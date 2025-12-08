74825477fcafa9df5dfb04e8e597b333dc397fdf
chukwukelu2023 <chukwukeluoo@gmail.com>
2025-12-08 12:54:03 +0100

    fix: check keys

--- infra/terraform/local.tf
+++ infra/terraform/local.tf
@@ -2,17 +2,40 @@ resource "time_sleep" "delay_configuration" {
   create_duration = "2m"
 }
 
-resource "local_file" "inventory" {
-  filename   = "${path.module}/../ansible/inventory.ini"
-  content    = <<-EOT
+# resource "local_file" "inventory" {
+#   filename   = "${path.module}/../ansible/inventory.ini"
+#   content    = <<-EOT
+#       [webserver]
+#       ${aws_instance.this.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file="~/.ssh/id_rsa"
+#       EOT
+  
+#   depends_on = [aws_instance.this, cloudflare_dns_record.dns_record,time_sleep.delay_configuration]
+
+
+# }
+
+
+resource "null_resource" "inventory" {
+
+   triggers = {
+    instance_ip = aws_instance.this.public_ip
+  }
+  depends_on = [
+    aws_instance.this,
+    cloudflare_dns_record.dns_record,
+    time_sleep.delay_configuration
+  ]
+
+  provisioner "local-exec" {
+    
+    command = <<-EOT
+      cat > ${path.module}/../ansible/inventory.ini <<EOF
       [webserver]
       ${aws_instance.this.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file="~/.ssh/id_rsa"
-      EOT
-  depends_on = [aws_instance.this, cloudflare_dns_record.dns_record,time_sleep.delay_configuration]
-
-
+      EOF
+    EOT
+  }
 }
-
 
 resource "null_resource" "provision" {
   provisioner "local-exec" {
